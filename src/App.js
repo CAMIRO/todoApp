5414cbfe507182e366f5266f034feaa90d148324
camiro <juancamilosc@hotmail.com>
2019-04-18 08:30:24 -0500

    todo list with delete feacture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
-
+import Todos from "./Todos";
 class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: "buy some milk" },
+      { id: 2, content: "play mario Kart" }
+    ]
+  };
+  deleteTodo = id => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos
+    });
+  };
   render() {
     return (
-      <div className="App">
-        <h1>My first React App!</h1>
-        <p>Welcome</p>
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
